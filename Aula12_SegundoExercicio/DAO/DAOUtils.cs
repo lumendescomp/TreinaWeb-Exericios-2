ced5b96ef505a32f4fdf8d091ae03419516c23f2
Lipe Mendes <82170079+lumendescomp@users.noreply.github.com>
2021-10-25 17:36:42 -0300

    Add files via upload

---
+++ Aula12_SegundoExercicio/DAO/DAOUtils.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12_SegundoExercicio.DAO
+{
+    public class DAOUtils
+    {
+        public static DbConnection GetConnection()
+        {
+            DbConnection conn = new MySqlConnection(@"Server=127.0.0.1;Database=Test;Uid=root;Pwd=1234;");
+            conn.Open();
+
+            return conn;
+        }
+
+        public static DbCommand GetCommand(DbConnection conn)
+        {
+            DbCommand command = conn.CreateCommand();
+            return command;
+        }
+
+        public static DbDataReader GetDataReader(DbCommand command)
+        {
+            return command.ExecuteReader();
+        }
+    }
+}
---
+++ Aula12_SegundoExercicio/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12_SegundoExercicio.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(int id = 0, string nome = "", double preco = 0.0, int quantidade = 0)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = quantidade;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} - Qdt: {2}", this.Nome, this.Preco.ToString("C"), this.Quantidade);
+        }
+    }
+}
