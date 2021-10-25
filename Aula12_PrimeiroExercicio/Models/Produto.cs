ec2dda591e024b4ac2966386a6245c5afa74dec0
Lipe Mendes <82170079+lumendescomp@users.noreply.github.com>
2021-10-25 17:35:28 -0300

    Add files via upload

---
+++ Aula12_PrimeiroExercicio/Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12_PrimeiroExercicio.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome = "", double preco = 0.0, int quantidade = 0)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = quantidade;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} - Qdt: {2}", this.Nome, this.Preco.ToString("C"), this.Quantidade);
+        }
+    }
+}
